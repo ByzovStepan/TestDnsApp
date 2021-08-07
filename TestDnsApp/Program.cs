92193824fadd88790bdce7d0ef389ba2d3d75211
Admin <Admin@ADMIN-PC>
2021-08-07 23:07:11 +0500

    Добавьте файлы проекта.

---
+++ TestDnsApp/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace TestDnsApp
+{
+    class Program
+    {
+        //Справка
+        static void Help()
+        {
+            Console.WriteLine(@"Аргументы при запуске программы:
+                            — задают глубину вложенности (-d или --depth)
+                            — показывают размер объектов (-s или --size)
+                            — справка по использованию (-? или --help)
+                        Используйте: TestDnsApp [dir] [-s] [-d depth]
+
+                        Примеры:
+                            TestDnsApp C:\Users\Admin\source\repos\TestDnsApp -s
+                            TestDnsApp \\ADMIN-PC\Shared
+                            TestDnsApp C:\Users\Admin\source\repos\ByzovCRM -s -d 4
+                            ");
+        }
+
+        static void Main(string[] args)
+        {
+            //Help();
+
+            string startDir = null;
+            bool showSize = false;
+            int maxDepth = int.MaxValue;
+
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                string arg = args[i];
+
+                if (arg == "-s" || arg == "--size")
+                    showSize = true;
+                else if (arg == "-d" || arg == "--depth")
+                {
+                    maxDepth = int.Parse(args[i + 1]);
+                    i++;
+                }
+                else if (arg.EndsWith("-?") || arg.EndsWith("--help"))
+                { 
+                    Help();
+                    return;
+                }  
+                else
+                {
+                    if (startDir == null)
+                        startDir = arg;
+                    else
+                    {
+                        Console.WriteLine("Неизвестный атрибут {0}", arg);
+                        Help();
+                        return;
+                    }
+                }
+            }
+
+            if (String.IsNullOrEmpty(startDir))
+            {
+                startDir = ".";
+            }
+
+            new Tree(startDir)
+            {
+                show_size = showSize,
+                max_depth = maxDepth,
+            }.Print();
+        }
+
+    }
+}
---
+++ TestDnsApp/Tree.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TestDnsApp
+{
+    public class Tree
+    {
+        public Tree(string startDir)
+        {
+            start_dir = startDir;
+        }
+        public void Print()
+        {
+            PrintTree(start_dir);
+        }
+
+        public string start_dir { get; }
+        public bool show_size { get; set; } = false;
+        public int max_depth { get; set; } = int.MaxValue;
+
+        public void PrintTree(string start_dir, string prefix = "", int depth = 0) 
+        {
+            if (depth >= max_depth)
+                return;
+
+            DirectoryInfo dirInfo = new DirectoryInfo(start_dir);
+            var fileItems = dirInfo.GetFileSystemInfos()
+                .Where(f => !f.Name.StartsWith("."))
+                .OrderBy(f => f.Name)
+                .ToList();
+
+
+            foreach (var fileItem in fileItems.Take(fileItems.Count-1))
+            {
+                Console.Write(prefix + "├── "); // U+02EB ˫
+                Console.Write($"{fileItem.Name} {GetSize(fileItem)} \n");
+                if (fileItem.isDirectory())
+                    PrintTree(fileItem.FullName, prefix + "│   ", depth + 1);
+            }
+
+            var lastFileItem = fileItems.LastOrDefault();
+            if (lastFileItem != null)
+            {
+                Console.Write(prefix + "└── ");
+                Console.Write($"{lastFileItem.Name} {GetSize(lastFileItem)} \n");
+                if (lastFileItem.isDirectory())
+                    PrintTree(lastFileItem.FullName, prefix + "    ", depth + 1);
+            }
+
+        }
+
+        public string GetSize(FileSystemInfo fileItem) 
+        {
+            if (show_size & !fileItem.isDirectory())
+            {
+                var path = fileItem.FullName;
+                var fileInfos = new FileInfo(path);
+                long fileSize = fileInfos.Length;
+
+                return ConverterSize(fileSize);
+            }
+            return "";
+        }
+
+        public string ConverterSize(long fileSize)
+        {
+            
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB" };
+            if (fileSize == 0)
+                return "(empty)";
+            long bytes = Math.Abs(fileSize);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return $"({(Math.Sign(fileSize) * num).ToString()} {suf[place]})";
+            
+            // Сначала попробовал простым способом, потом стало интересно получить точное значение
+            /*
+            long actualSize;
+
+            if (fileSize == 0)
+                return "(empty)";
+            else if (fileSize >= 1073741824) 
+            {
+                actualSize = fileSize / 1073741824;
+                return $"({actualSize} GB)";
+            }
+            else if (fileSize >= 1048576)
+            {
+                actualSize = fileSize / 1048576;
+                return $"({actualSize} MB)";
+            }
+            else if (fileSize >= 1024)
+            {
+                actualSize = fileSize / 1024;
+                return $"({actualSize} KB)";
+            }
+            else
+            {
+                return $"({fileSize} B)";
+            }
+            */
+        }
+        
+
+    }
+
+    public static class FileSystemInfoExtensions
+    {
+        public static bool isDirectory(this FileSystemInfo fileItem) =>
+            (fileItem.Attributes & FileAttributes.Directory) == FileAttributes.Directory;
+    }
+}
